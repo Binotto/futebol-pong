ef4117ad7aeab4adde024b56f71353f75a1e1b85
Binotto <matheus.binotto@hotmail.com>
2021-11-04 19:12:56 -0700

    improving player 1 moves

--- main.py
+++ main.py
@@ -9,10 +9,28 @@ title = pygame.display.set_caption("Futebal Pong")
 field = pygame.image.load("assets/field.png")
 player1 = pygame.image.load("assets/player1.png")
 player1_y = 310
+player1_moveup = False
+player1_movedown = False
 player2 = pygame.image.load("assets/player2.png")
 ball = pygame.image.load("assets/ball.png")
 ball_x = 617
 ball_y = 337
+
+
+def move_player():
+    global player1_y
+
+    if player1_moveup:
+        player1_y -= 10
+    else:
+        player1_y += 0
+
+    if player1_movedown:
+        player1_y += 10
+    else:
+        player1_y += 0
+
+
 
 
 def move_ball():
@@ -38,9 +56,17 @@ while loop:
         if events.type == pygame.KEYDOWN:
             if events.key == pygame.K_w:
                 player1_y -= 10
+                player1_moveup = True
             if events.key == pygame.K_s:
                 player1_y += 10
+                player1_movedown = True
+        if events.type == pygame.KEYUP:
+            if events.key == pygame.K_w:
+                player1_moveup = False
+            if events.key == pygame.K_s:
+                player1_movedown = False
 
     draw()
     move_ball()
+    move_player()
     pygame.display.update()
