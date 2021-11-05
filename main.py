8ac0c916c55f5ddf24db36280ac4ec04158a0726
Binotto <matheus.binotto@hotmail.com>
2021-11-04 18:33:22 -0700

    adding game background

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ pygame.init()
 window = pygame.display.set_mode([1280, 720])
 title = pygame.display.set_caption("Futebal Pong")
 
+
+field = pygame.image.load("assets/field.png")
+window.blit(field, (0, 0))
+
+
 loop = True
 while loop:
 
