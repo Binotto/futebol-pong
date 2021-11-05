266d9d3b526268fea085047184a339d5039ee062
Binotto <matheus.binotto@hotmail.com>
2021-11-04 18:43:25 -0700

    Adding players

--- main.py
+++ main.py
@@ -9,6 +9,15 @@ title = pygame.display.set_caption("Futebal Pong")
 field = pygame.image.load("assets/field.png")
 window.blit(field, (0, 0))
 
+player1 = pygame.image.load("assets/player1.png")
+window.blit(player1, (50, 310))
+
+player2 = pygame.image.load("assets/player2.png")
+window.blit(player2, (1150, 310))
+
+ball = pygame.image.load("assets/ball.png")
+window.blit(ball, (617, 317))
+
 
 loop = True
 while loop:
